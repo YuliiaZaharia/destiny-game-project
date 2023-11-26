e9f69469c9ffea590838e9d42d68d2b01dd52c97
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-26 17:12:34 +0300

    new function married

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ function mrBig() {
     text.textContent = "WILL THEY GET MARRIED?";
     buttonOne.textContent = "YES, BUT LATER"; 
     buttonTwo.textContent = "NEVER"; 
+
+    buttonOne.addEventListener("click", married);
+}
+
+function married() {
+    image.setAttribute("src", "https://www.usatoday.com/gcdn/-mm-/1062cece51fd908e0c01f74b75c6b7b4229c4b22/c=0-195-3744-2301/local/-/media/2015/12/14/USATODAY/usatsports/xxx_sex_city_2_mov__6746_35507631.jpg?width=1320&height=744&fit=crop&format=pjpg&auto=webp"); 
+    text.textContent = "THEY WILL GET MARRIED, BUT WITHOUT KIDS";
+    buttonOne.style = "display: none";
+    buttonTwo.style = "display: none";
 }
