304af675b1764a1927aa3bd2bfcc31bd67437f34
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-26 17:02:49 +0300

    used method setAttribute() to change image, and textContent to change text of h2

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ buttonOne.addEventListener("click", mrBig);
 
 function mrBig() {
     image.setAttribute("src", "https://stylecaster.com/wp-content/uploads/2014/10/mr-big-carrie.jpg?w=500&h=339&crop=1"); 
+    text.textContent = "WILL THEY GET MARRIED?";
+    
 }
