a976409d3afaf7beaae82bd64d868243baecd7d2
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-26 17:00:45 +0300

    new function mrBig

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const image = document.querySelector("#myImage"); 
+const image = document.querySelector("#myPicture"); 
 const text = document.querySelector("h2"); 
 
 const buttonOne = document.querySelector("#one");
 const buttonTwo = document.querySelector("#two"); 
 
+buttonOne.addEventListener("click", mrBig); 
+
+function mrBig() {
+    image.setAttribute("src", "https://stylecaster.com/wp-content/uploads/2014/10/mr-big-carrie.jpg?w=500&h=339&crop=1"); 
+}
