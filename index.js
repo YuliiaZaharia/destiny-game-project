e9c6d086aa4c3428d7a931ba4efeb6f0ef36a831
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-26 16:48:48 +0300

    connected to buttons with querySelector()

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const image = document.querySelector("#myImage"); 
 const text = document.querySelector("h2"); 
 
+const buttonOne = document.querySelector("#one");
+const buttonTwo = document.querySelector("#two"); 
+
